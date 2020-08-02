05af1d722f1811fcb93f21e18d971415c52351e0
Gustavo <gustavo.athenas13@gmail.com>
2020-08-02 20:21:04 -0300

    configuração da tela e imagem da nave

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    """ """
+
+    def __init__(self):
+        """ """
+        # Configuração de Tela
+        self.screen_width = 700
+        self.screen_height = 500
+        self.bg_color = (128, 128, 128)
